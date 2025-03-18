63c5a87ef102eb32756ec92fb9176159801222ce
desivar <jillian_desire@yahoo.es>
2025-03-17 18:30:26 -0600

    .gitignore

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+// Correct path to the router file
+app.use('/', require('./routes'));
 
 app.listen(port, () => {
-    console.log(`Server on port ${port}`)});
-
-
-    
+  console.log(`Server on port ${port}`);
+});
