605557f04a480e63da27f0370fdef428072ca47c
desivar <jillian_desire@yahoo.es>
2025-03-20 10:36:24 -0600

    upadted

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
 const express = require('express');
-const db = require('./data/db');
-const routes = require('./routes');
+const db = require('./data/db');// Import the db.js module
+const routes = require('./routes'); // Import your routes
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Move the / route here
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Contacts</h1>
-    <ul id="contactsList"></ul>
-    <script>
-      fetch('/contacts')
-        .then(response => response.json())
-        .then(data => {
-          const contactsList = document.getElementById('contactsList');
-          data.forEach(contact => {
-            const listItem = document.createElement('li');
-            listItem.textContent = \`\${contact.firstName} \${contact.lastName} (\${contact.email})\`;
-            contactsList.appendChild(listItem);
-          });
-        });
-    </script>
-  `);
-});
 
 app.use(routes);
 
