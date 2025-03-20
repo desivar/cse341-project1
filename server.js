997e9675a64c98ab885b3076ac1daed23a88b69d
desivar <jillian_desire@yahoo.es>
2025-03-20 06:10:28 -0600

    Add route to display contacts in HTMl inserver.js

--- server.js
+++ server.js
@@ -20,3 +20,25 @@ db.intDb((err, database) => {
     console.log(`Server is running on port ${port}`);
   });
 });
+// ... (your existing code)
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Contacts</h1>
+    <ul id="contactsList"></ul>
+    <script>
+      fetch('/contacts')
+        .then(response => response.json())
+        .then(data => {
+          const contactsList = document.getElementById('contactsList');
+          data.forEach(contact => {
+            const listItem = document.createElement('li');
+            listItem.textContent = \`\${contact.firstName} \${contact.lastName} (\${contact.email})\`;
+            contactsList.appendChild(listItem);
+          });
+        });
+    </script>
+  `);
+});
+
+// ... (your db.intDb and app.listen)
