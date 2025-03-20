4728c44426082dbfa9b45ba1b9687c1587771e45
desivar <jillian_desire@yahoo.es>
2025-03-20 08:48:32 -0600

    Add public folder and frontend files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./data/db');
 const routes = require('./routes');
 const dotenv = require('dotenv');
-const path = require('path'); // Add this line
+const path = require('path');
 dotenv.config();
 
 const app = express();
@@ -11,26 +11,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public'))); // Add this line
 
 app.use(routes);
-
-// Move the / route here
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Contacts</h1>
-    <ul id="contactsList"></ul>
-    <script>
-      fetch('/contacts')
-        .then(response => response.json())
-        .then(data => {
-          const contactsList = document.getElementById('contactsList');
-          data.forEach(contact => {
-            const listItem = document.createElement('li');
-            listItem.textContent = \`\${contact.firstName} \${contact.lastName} (\${contact.email})\`;
-            contactsList.appendChild(listItem);
-          });
-        });
-    </script>
-  `);
-});
 
 db.intDb((err, database) => {
   if (err) {
